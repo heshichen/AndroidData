cbdb8f1a6962ac8fa1a776fef10a2ddd9e51dabc
tianlongrenlufei <793308186@qq.com>
2018-09-12 15:17:41 +0800

    练习

---
+++ 练习/FirstApp2/app/src/main/java/com/example/admin/firstapp/activity/ViewFlipperTset.java
@@ -0,0 +1,10 @@
+package com.example.admin.firstapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by admin on 2018/9/11.
+ */
+
+public class ViewFlipperTset extends AppCompatActivity {
+}
